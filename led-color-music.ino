f772325fc6449a44b9b383049adbfd9b21519805
Roman Skvazh <roman.skvazh@gmail.com>
2023-01-20 19:36:22 +0300

    refactor

--- led-color-music.ino
+++ led-color-music.ino
@@ -18,9 +18,20 @@
   Кнопка 0: fullLowPass() для микрофона
 */
 
+// Sketch uses 19088 bytes (62%) of program storage space. Maximum is 30720 bytes.
+// Global variables use 1215 bytes (59%) of dynamic memory, leaving 833 bytes for local variables. Maximum is 2048 bytes.
+
 // ----- настройка ИК пульта
 #define REMOTE_TYPE 1  // 0 - без пульта, 1 - пульт от WAVGAT, 2 - пульт от KEYES, 3 - кастомный пульт
 // система может работать С ЛЮБЫМ ИК ПУЛЬТОМ (практически). Коды для своего пульта можно задать начиная со строки 160 в прошивке. Коды пультов определяются скетчем IRtest_2.0, читай инструкцию
+#include "Remote.h"
+
+
+const byte MODE_GRADIENT = 0;  // old 6:0
+const byte MODE_SOLID = 1;     // old 6:2
+const byte MODE_STATIC = 2;
+const byte MODE_STROBE = 3;
+const byte MODE_MUSIC_RAINBOW = 4;
 
 // ----- настройки параметров
 #define KEEP_SETTINGS 0   // хранить ВСЕ настройки в энергонезависимой памяти
@@ -52,7 +63,6 @@ byte BRIGHTNESS = 255;   // яркость по умолчанию (0 - 255)
 float RAINBOW_STEP = 5.00;  // шаг изменения цвета радуги
 
 // ----- отрисовка
-#define MODE 6       // режим при запуске
 #define MAIN_LOOP 5  // период основного цикла отрисовки (по умолчанию 5)
 
 // ----- сигнал
@@ -116,73 +126,8 @@ byte HUE_STEP = 5;
   HUE_PINK
 */
 
-// ----- КНОПКИ ПУЛЬТА WAVGAT -----
-#if REMOTE_TYPE == 1
-#define BUTT_UP 0xF39EEBAD
-#define BUTT_DOWN 0xC089F6AD
-#define BUTT_LEFT 0xE25410AD
-#define BUTT_RIGHT 0x14CE54AD
-#define BUTT_OK 0x297C76AD
-#define BUTT_1 0x4E5BA3AD
-#define BUTT_2 0xE51CA6AD
-#define BUTT_3 0xE207E1AD
-#define BUTT_4 0x517068AD
-#define BUTT_5 0x1B92DDAD
-#define BUTT_6 0xAC2A56AD
-#define BUTT_7 0x5484B6AD
-#define BUTT_8 0xD22353AD
-#define BUTT_9 0xDF3F4BAD
-#define BUTT_0 0xF08A26AD
-#define BUTT_STAR 0x68E456AD
-#define BUTT_HASH 0x151CD6AD
-#endif
-
-// ----- КНОПКИ ПУЛЬТА KEYES -----
-#if REMOTE_TYPE == 2
-#define BUTT_UP 0xE51CA6AD
-#define BUTT_DOWN 0xD22353AD
-#define BUTT_LEFT 0x517068AD
-#define BUTT_RIGHT 0xAC2A56AD
-#define BUTT_OK 0x1B92DDAD
-#define BUTT_1 0x68E456AD
-#define BUTT_2 0xF08A26AD
-#define BUTT_3 0x151CD6AD
-#define BUTT_4 0x18319BAD
-#define BUTT_5 0xF39EEBAD
-#define BUTT_6 0x4AABDFAD
-#define BUTT_7 0xE25410AD
-#define BUTT_8 0x297C76AD
-#define BUTT_9 0x14CE54AD
-#define BUTT_0 0xC089F6AD
-#define BUTT_STAR 0xAF3F1BAD
-#define BUTT_HASH 0x38379AD
-#endif
-
-// ----- КНОПКИ СВОЕГО ПУЛЬТА -----
-#if REMOTE_TYPE == 3
-#define BUTT_UP 0xE51CA6AD
-#define BUTT_DOWN 0xD22353AD
-#define BUTT_LEFT 0x517068AD
-#define BUTT_RIGHT 0xAC2A56AD
-#define BUTT_OK 0x1B92DDAD
-#define BUTT_1 0x68E456AD
-#define BUTT_2 0xF08A26AD
-#define BUTT_3 0x151CD6AD
-#define BUTT_4 0x18319BAD
-#define BUTT_5 0xF39EEBAD
-#define BUTT_6 0x4AABDFAD
-#define BUTT_7 0xE25410AD
-#define BUTT_8 0x297C76AD
-#define BUTT_9 0x14CE54AD
-#define BUTT_0 0xC089F6AD
-#define BUTT_STAR 0xAF3F1BAD  // *
-#define BUTT_HASH 0x38379AD   // #
-#endif
-
 
 // ------------------------------ ДЛЯ РАЗРАБОТЧИКОВ --------------------------------
-#define MODE_AMOUNT 9  // количество режимов
-
 #define STRIPE NUM_LEDS / 5
 float freq_to_stripe = NUM_LEDS / 40;  // /2 так как симметрия, и /20 так как 20 частот
 
@@ -228,7 +173,7 @@ int RcurrentLevel, LcurrentLevel;
 int colorMusic[3];
 float colorMusic_f[3], colorMusic_aver[3];
 boolean colorMusicFlash[3], strobeUp_flag, strobeDwn_flag;
-byte currentMode = MODE;
+byte currentMode = MODE_GRADIENT;
 int thisBright[3], strobe_bright = 0;
 unsigned int light_time = STROBE_PERIOD * STROBE_DUTY / 100;
 volatile boolean ir_flag;
@@ -344,163 +289,165 @@ void setup() {
 }
 
 void loop() {
+
 #if REMOTE_TYPE != 0
   remoteTick();  // опрос ИК пульта
 #endif
-  mainLoop();    // главный цикл обработки и отрисовки
+
+  if (ONstate) {
+    mainLoop();    // главный цикл обработки и отрисовки
+  }
+  
   eepromTick();  // проверка не пора ли сохранить настройки
 }
 
 void mainLoop() {
-  // главный цикл отрисовки
-  if (ONstate) {
-    if (millis() - main_timer > MAIN_LOOP) {
-      // сбрасываем значения
-      RsoundLevel = 0;
-      LsoundLevel = 0;
+  if (millis() - main_timer > MAIN_LOOP) {
+    // сбрасываем значения
+    RsoundLevel = 0;
+    LsoundLevel = 0;
 
-      // перваые два режима - громкость (VU meter)
-      if (currentMode == 0 || currentMode == 1) {
-        for (byte i = 0; i < 100; i++) {                   // делаем 100 измерений
-          RcurrentLevel = analogRead(SOUND_R);             // с правого
-          if (!MONO) LcurrentLevel = analogRead(SOUND_L);  // и левого каналов
+    // перваые два режима - громкость (VU meter)
+    if (currentMode == 0 || currentMode == 1) {
+      for (byte i = 0; i < 100; i++) {                   // делаем 100 измерений
+        RcurrentLevel = analogRead(SOUND_R);             // с правого
+        if (!MONO) LcurrentLevel = analogRead(SOUND_L);  // и левого каналов
 
-          if (RsoundLevel < RcurrentLevel) RsoundLevel = RcurrentLevel;  // ищем максимальное
-          if (!MONO)
-            if (LsoundLevel < LcurrentLevel) LsoundLevel = LcurrentLevel;  // ищем максимальное
-        }
-
-
-        // фильтруем по нижнему порогу шумов
-        RsoundLevel = map(RsoundLevel, LOW_PASS, 1023, 0, 500);
-        if (!MONO) LsoundLevel = map(LsoundLevel, LOW_PASS, 1023, 0, 500);
-
-        // ограничиваем диапазон
-        RsoundLevel = constrain(RsoundLevel, 0, 500);
-        if (!MONO) LsoundLevel = constrain(LsoundLevel, 0, 500);
-
-        // возводим в степень (для большей чёткости работы)
-        RsoundLevel = pow(RsoundLevel, EXP);
-        if (!MONO) LsoundLevel = pow(LsoundLevel, EXP);
-
-        // фильтр
-        RsoundLevel_f = RsoundLevel * SMOOTH + RsoundLevel_f * (1 - SMOOTH);
-        if (!MONO) LsoundLevel_f = LsoundLevel * SMOOTH + LsoundLevel_f * (1 - SMOOTH);
-
-        if (MONO) LsoundLevel_f = RsoundLevel_f;  // если моно, то левый = правому
-
-        // заливаем "подложку", если яркость достаточная
-        if (EMPTY_BRIGHT > 5) {
-          for (int i = 0; i < NUM_LEDS; i++)
-            leds[i] = CHSV(EMPTY_COLOR, 255, EMPTY_BRIGHT);
-        }
-
-        // если значение выше порога - начинаем самое интересное
-        if (RsoundLevel_f > 15 && LsoundLevel_f > 15) {
-
-          // расчёт общей средней громкости с обоих каналов, фильтрация.
-          // Фильтр очень медленный, сделано специально для автогромкости
-          averageLevel = (float)(RsoundLevel_f + LsoundLevel_f) / 2 * averK + averageLevel * (1 - averK);
-
-          // принимаем максимальную громкость шкалы как среднюю, умноженную на некоторый коэффициент MAX_COEF
-          maxLevel = (float)averageLevel * MAX_COEF;
-
-          // преобразуем сигнал в длину ленты (где MAX_CH это половина количества светодиодов)
-          Rlenght = map(RsoundLevel_f, 0, maxLevel, 0, MAX_CH);
-          Llenght = map(LsoundLevel_f, 0, maxLevel, 0, MAX_CH);
-
-          // ограничиваем до макс. числа светодиодов
-          Rlenght = constrain(Rlenght, 0, MAX_CH);
-          Llenght = constrain(Llenght, 0, MAX_CH);
-
-          animation();  // отрисовать
-        }
+        if (RsoundLevel < RcurrentLevel) RsoundLevel = RcurrentLevel;  // ищем максимальное
+        if (!MONO)
+          if (LsoundLevel < LcurrentLevel) LsoundLevel = LcurrentLevel;  // ищем максимальное
       }
 
-      // 3-5 режим - цветомузыка
-      if (currentMode == 2 || currentMode == 3 || currentMode == 4 || currentMode == 7 || currentMode == 8) {
-        analyzeAudio();
-        colorMusic[0] = 0;
-        colorMusic[1] = 0;
-        colorMusic[2] = 0;
-        for (int i = 0; i < 32; i++) {
-          if (fht_log_out[i] < SPEKTR_LOW_PASS) fht_log_out[i] = 0;
-        }
-        // низкие частоты, выборка со 2 по 5 тон (0 и 1 зашумленные!)
-        for (byte i = 2; i < 6; i++) {
-          if (fht_log_out[i] > colorMusic[0]) colorMusic[0] = fht_log_out[i];
-        }
-        // средние частоты, выборка с 6 по 10 тон
-        for (byte i = 6; i < 11; i++) {
-          if (fht_log_out[i] > colorMusic[1]) colorMusic[1] = fht_log_out[i];
-        }
-        // высокие частоты, выборка с 11 по 31 тон
-        for (byte i = 11; i < 32; i++) {
-          if (fht_log_out[i] > colorMusic[2]) colorMusic[2] = fht_log_out[i];
-        }
-        freq_max = 0;
-        for (byte i = 0; i < 30; i++) {
-          if (fht_log_out[i + 2] > freq_max) freq_max = fht_log_out[i + 2];
-          if (freq_max < 5) freq_max = 5;
 
-          if (freq_f[i] < fht_log_out[i + 2]) freq_f[i] = fht_log_out[i + 2];
-          if (freq_f[i] > 0) freq_f[i] -= LIGHT_SMOOTH;
-          else freq_f[i] = 0;
-        }
-        freq_max_f = freq_max * averK + freq_max_f * (1 - averK);
-        for (byte i = 0; i < 3; i++) {
-          colorMusic_aver[i] = colorMusic[i] * averK + colorMusic_aver[i] * (1 - averK);        // общая фильтрация
-          colorMusic_f[i] = colorMusic[i] * SMOOTH_FREQ + colorMusic_f[i] * (1 - SMOOTH_FREQ);  // локальная
-          if (colorMusic_f[i] > ((float)colorMusic_aver[i] * MAX_COEF_FREQ)) {
-            thisBright[i] = 255;
-            colorMusicFlash[i] = true;
-            running_flag[i] = true;
-          } else colorMusicFlash[i] = false;
-          if (thisBright[i] >= 0) thisBright[i] -= SMOOTH_STEP;
-          if (thisBright[i] < EMPTY_BRIGHT) {
-            thisBright[i] = EMPTY_BRIGHT;
-            running_flag[i] = false;
-          }
-        }
-        animation();
+      // фильтруем по нижнему порогу шумов
+      RsoundLevel = map(RsoundLevel, LOW_PASS, 1023, 0, 500);
+      if (!MONO) LsoundLevel = map(LsoundLevel, LOW_PASS, 1023, 0, 500);
+
+      // ограничиваем диапазон
+      RsoundLevel = constrain(RsoundLevel, 0, 500);
+      if (!MONO) LsoundLevel = constrain(LsoundLevel, 0, 500);
+
+      // возводим в степень (для большей чёткости работы)
+      RsoundLevel = pow(RsoundLevel, EXP);
+      if (!MONO) LsoundLevel = pow(LsoundLevel, EXP);
+
+      // фильтр
+      RsoundLevel_f = RsoundLevel * SMOOTH + RsoundLevel_f * (1 - SMOOTH);
+      if (!MONO) LsoundLevel_f = LsoundLevel * SMOOTH + LsoundLevel_f * (1 - SMOOTH);
+
+      if (MONO) LsoundLevel_f = RsoundLevel_f;  // если моно, то левый = правому
+
+      // заливаем "подложку", если яркость достаточная
+      if (EMPTY_BRIGHT > 5) {
+        for (int i = 0; i < NUM_LEDS; i++)
+          leds[i] = CHSV(EMPTY_COLOR, 255, EMPTY_BRIGHT);
       }
-      if (currentMode == 5) {
-        if ((long)millis() - strobe_timer > STROBE_PERIOD) {
-          strobe_timer = millis();
-          strobeUp_flag = true;
-          strobeDwn_flag = false;
-        }
-        if ((long)millis() - strobe_timer > light_time) {
-          strobeDwn_flag = true;
-        }
-        if (strobeUp_flag) {                 // если настало время пыхнуть
-          if (strobe_bright < 255)           // если яркость не максимальная
-            strobe_bright += STROBE_SMOOTH;  // увелчить
-          if (strobe_bright > 255) {         // если пробили макс. яркость
-            strobe_bright = 255;             // оставить максимум
-            strobeUp_flag = false;           // флаг опустить
-          }
-        }
 
-        if (strobeDwn_flag) {                // гаснем
-          if (strobe_bright > 0)             // если яркость не минимальная
-            strobe_bright -= STROBE_SMOOTH;  // уменьшить
-          if (strobe_bright < 0) {           // если пробили мин. яркость
-            strobeDwn_flag = false;
-            strobe_bright = 0;  // оставить 0
-          }
-        }
-        animation();
+      // если значение выше порога - начинаем самое интересное
+      if (RsoundLevel_f > 15 && LsoundLevel_f > 15) {
+
+        // расчёт общей средней громкости с обоих каналов, фильтрация.
+        // Фильтр очень медленный, сделано специально для автогромкости
+        averageLevel = (float)(RsoundLevel_f + LsoundLevel_f) / 2 * averK + averageLevel * (1 - averK);
+
+        // принимаем максимальную громкость шкалы как среднюю, умноженную на некоторый коэффициент MAX_COEF
+        maxLevel = (float)averageLevel * MAX_COEF;
+
+        // преобразуем сигнал в длину ленты (где MAX_CH это половина количества светодиодов)
+        Rlenght = map(RsoundLevel_f, 0, maxLevel, 0, MAX_CH);
+        Llenght = map(LsoundLevel_f, 0, maxLevel, 0, MAX_CH);
+
+        // ограничиваем до макс. числа светодиодов
+        Rlenght = constrain(Rlenght, 0, MAX_CH);
+        Llenght = constrain(Llenght, 0, MAX_CH);
+
+        animation();  // отрисовать
       }
-      if (currentMode == 6) animation();
-
-      if (!IRLremote.receiving())  // если на ИК приёмник не приходит сигнал (без этого НЕ РАБОТАЕТ!)
-        FastLED.show();            // отправить значения на ленту
-
-      if (currentMode != 7)     // 7 режиму не нужна очистка!!!
-        FastLED.clear();      // очистить массив пикселей
-      main_timer = millis();  // сбросить таймер
     }
+
+    // 3-5 режим - цветомузыка
+    if (currentMode == 2 || currentMode == 3 || currentMode == 4 || currentMode == 7 || currentMode == 8) {
+      analyzeAudio();
+      colorMusic[0] = 0;
+      colorMusic[1] = 0;
+      colorMusic[2] = 0;
+      for (int i = 0; i < 32; i++) {
+        if (fht_log_out[i] < SPEKTR_LOW_PASS) fht_log_out[i] = 0;
+      }
+      // низкие частоты, выборка со 2 по 5 тон (0 и 1 зашумленные!)
+      for (byte i = 2; i < 6; i++) {
+        if (fht_log_out[i] > colorMusic[0]) colorMusic[0] = fht_log_out[i];
+      }
+      // средние частоты, выборка с 6 по 10 тон
+      for (byte i = 6; i < 11; i++) {
+        if (fht_log_out[i] > colorMusic[1]) colorMusic[1] = fht_log_out[i];
+      }
+      // высокие частоты, выборка с 11 по 31 тон
+      for (byte i = 11; i < 32; i++) {
+        if (fht_log_out[i] > colorMusic[2]) colorMusic[2] = fht_log_out[i];
+      }
+      freq_max = 0;
+      for (byte i = 0; i < 30; i++) {
+        if (fht_log_out[i + 2] > freq_max) freq_max = fht_log_out[i + 2];
+        if (freq_max < 5) freq_max = 5;
+
+        if (freq_f[i] < fht_log_out[i + 2]) freq_f[i] = fht_log_out[i + 2];
+        if (freq_f[i] > 0) freq_f[i] -= LIGHT_SMOOTH;
+        else freq_f[i] = 0;
+      }
+      freq_max_f = freq_max * averK + freq_max_f * (1 - averK);
+      for (byte i = 0; i < 3; i++) {
+        colorMusic_aver[i] = colorMusic[i] * averK + colorMusic_aver[i] * (1 - averK);        // общая фильтрация
+        colorMusic_f[i] = colorMusic[i] * SMOOTH_FREQ + colorMusic_f[i] * (1 - SMOOTH_FREQ);  // локальная
+        if (colorMusic_f[i] > ((float)colorMusic_aver[i] * MAX_COEF_FREQ)) {
+          thisBright[i] = 255;
+          colorMusicFlash[i] = true;
+          running_flag[i] = true;
+        } else colorMusicFlash[i] = false;
+        if (thisBright[i] >= 0) thisBright[i] -= SMOOTH_STEP;
+        if (thisBright[i] < EMPTY_BRIGHT) {
+          thisBright[i] = EMPTY_BRIGHT;
+          running_flag[i] = false;
+        }
+      }
+      animation();
+    }
+    if (currentMode == 5) {
+      if ((long)millis() - strobe_timer > STROBE_PERIOD) {
+        strobe_timer = millis();
+        strobeUp_flag = true;
+        strobeDwn_flag = false;
+      }
+      if ((long)millis() - strobe_timer > light_time) {
+        strobeDwn_flag = true;
+      }
+      if (strobeUp_flag) {                 // если настало время пыхнуть
+        if (strobe_bright < 255)           // если яркость не максимальная
+          strobe_bright += STROBE_SMOOTH;  // увелчить
+        if (strobe_bright > 255) {         // если пробили макс. яркость
+          strobe_bright = 255;             // оставить максимум
+          strobeUp_flag = false;           // флаг опустить
+        }
+      }
+
+      if (strobeDwn_flag) {                // гаснем
+        if (strobe_bright > 0)             // если яркость не минимальная
+          strobe_bright -= STROBE_SMOOTH;  // уменьшить
+        if (strobe_bright < 0) {           // если пробили мин. яркость
+          strobeDwn_flag = false;
+          strobe_bright = 0;  // оставить 0
+        }
+      }
+      animation();
+    }
+    if (currentMode == 6) animation();
+
+    if (!IRLremote.receiving())  // если на ИК приёмник не приходит сигнал (без этого НЕ РАБОТАЕТ!)
+      FastLED.show();            // отправить значения на ленту
+
+    if (currentMode != 7)   // 7 режиму не нужна очистка!!!
+      FastLED.clear();      // очистить массив пикселей
+    main_timer = millis();  // сбросить таймер
   }
 }
 
