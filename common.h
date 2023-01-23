98e10271812c9ac762214e3d79b156d99836315e
Roman Skvazh <roman.skvazh@gmail.com>
2023-01-24 00:46:19 +0300

    Create common.h

---
+++ common.h
@@ -0,0 +1,21 @@
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+
+// typedef int (*StepFunction)(int a , int b );
+typedef void (*StepFunction)(CRGB *leds, int num_leds, unsigned long elapsed);
+
+struct Modes {
+  StepFunction step;
+};
+
+int smartIncr(int value, int incr_step, int mininmum, int maximum) {
+  int val_buf = value + incr_step;
+  val_buf = constrain(val_buf, mininmum, maximum);
+  return val_buf;
+}
+
+float smartIncrFloat(float value, float incr_step, float mininmum, float maximum) {
+  float val_buf = value + incr_step;
+  val_buf = constrain(val_buf, mininmum, maximum);
+  return val_buf;
+}
